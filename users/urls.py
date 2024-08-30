523a5f52776253e19dfc6b2e2703f6c812f857db
harris-dhanraj <harrisdhanraj246@gmail.com>
2024-08-30 18:07:34 +0530

    Pagination

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('accounts/', include("django.contrib.auth.urls")),
     path('signup/', signup, name="signup"),
     path('event/<int:id>/', views.event_detail, name='event_detail'),
+    path('events/<str:event_tag>/', views.events_by_tag, name='events_by_tag'),
     path('search/', views.search, name='search'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,17 +4,41 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import User
 from .models import Event
+from django.core.paginator import Paginator
+
+from django.db.models import Count
 
 def home(request):
     '''Home page with all the events'''
-    events = Event.objects.all()
-    cities = Event.objects.values_list('city', flat=True).distinct()
-    return render(request, 'home.html', {'events': events, 'cities': cities})
+     # Retrieve all events and shuffe them
+    events = Event.objects.all().order_by('?')[:5]
+     # Retrieve all unique event tags
+    tags = Event.objects.values('event_tag')
+
+    events_by_tag = {}
+    for tag in tags:
+        events_by_tag[tag['event_tag']] = Event.objects.filter(event_tag=tag['event_tag'])[:5]
+    
+    return render(request, 'home.html', {'events': events, 'events_by_tag': events_by_tag})
 
 def event_detail(request, id):
     '''Event Detail page which returns the details of the event '''
     event = Event.objects.get(id=id)
     return render(request, 'event_detail.html', {'event': event})
+
+def events_by_tag(request, event_tag):
+    '''Display all events under a specific event_tag'''
+    if event_tag == 'Upcoming':
+        events = Event.objects.all().order_by('?')
+    else:
+        events = Event.objects.filter(event_tag=event_tag)
+    
+    # Set up pagination (e.g., 5 events per page)
+    paginator = Paginator(events, 6) 
+    page_number = request.GET.get('page')
+    events = paginator.get_page(page_number)
+    
+    return render(request, 'events_by_tag.html', {'events': events, 'event_tag': event_tag})
 
 def search(request):
     '''Searching Event'''
